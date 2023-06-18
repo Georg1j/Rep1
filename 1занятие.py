2e8a344410f6ff610a41e92f4960cb723f9aafd4
Георгий <egor2018smirnov@mail.ru>
2023-06-18 13:34:52 +0300

    Изменение 2

--- 1занятие.py
+++ 1занятие.py
@@ -22,9 +22,4 @@ def strcounter(s): # O(N)
     syms_counter = {}
     for sym in s:
         syms_counter[sym] = syms_counter.get(sym, 0) + 1
-    for sym, count in syms_counter.items():
-        print(sym, count)
-
-
-
-strcounter('aaaaaadddddfggg')
+print('edduhduhihiis')
